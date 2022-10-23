70ec5b0e9221e3baef7e13a9ac13f5fc0f170f85
mykalebriggs <116503583+mykalebriggs@users.noreply.github.com>
2022-10-24 00:01:39 +0900

    Add files via upload

---
+++ program.js
@@ -0,0 +1,3 @@
+let Name = "Mike"; //You can put your name here
+let Classes = ["JBE", "ECON", "OB1", "JLI"];
+console.log("Hello, my name is " + Name + " and I am taking " + Classes[0] + ", " + Classes[1] + ", " + Classes[2] + ", and " + Classes[3]); 
